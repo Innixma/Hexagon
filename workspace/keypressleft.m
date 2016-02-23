95dc827f91839cb339669247b05be80604a9c2f8
Josh Glendenning <joshglendenning@gmail.com>
2016-02-22 18:14:20 -0600

    Cleaned up tabbing

--- workspace/keypressleft.m
+++ workspace/keypressleft.m
@@ -1,4 +1,4 @@
 function keypressleft()
-    robot = java.awt.Robot;
+	robot = java.awt.Robot;
 	robot.keyPress(java.awt.event.KeyEvent.VK_LEFT);
 end
--- workspace/keypressright.m
+++ workspace/keypressright.m
@@ -1,4 +1,4 @@
 function keypressright()
-    robot = java.awt.Robot;
+	robot = java.awt.Robot;
 	robot.keyPress(java.awt.event.KeyEvent.VK_RIGHT);
 end
